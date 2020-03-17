819266089919bcc94bf6b7c170909bdefa01913d
tehblasian <jeremiahdavid.wreh@gmail.com>
2020-03-17 17:29:44 -0400

    [#10] Calculate class distribution and plot bar chart

--- data_exploration/class_distribution.py
+++ data_exploration/class_distribution.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pyspark
 from pyspark.sql import SparkSession
 
@@ -20,7 +21,26 @@ def init_spark():
 def class_distribution():
     spark = init_spark()
     df = spark.read.format('mongo').load()
-    df.printSchema()
+    
+    distribution = df.groupBy('label').count().collect()
+    show_bar_plot(distribution)
+
+
+def show_bar_plot(distribution):
+    counts = [row['count'] for row in distribution]
+    labels = [row['label'] for row in distribution]
+
+    y_pos = np.arange(len(labels))
+    plt.figure(figsize=(10,5))
+    plt.bar(y_pos, counts, color=['grey', 'black', 'red', 'green'])
+    plt.xticks(y_pos, labels)
+
+    plt.title('Distribution of labels for posts in r/AITA')
+    plt.ylabel('Count')
+    plt.xlabel('Label')
+
+    plt.show()
+
 
 if __name__ == '__main__':
     class_distribution()

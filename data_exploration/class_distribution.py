091d103e9d159e54fc93f12b7ccaa8186f22aba2
tehblasian <jeremiahdavid.wreh@gmail.com>
2020-03-18 21:00:15 -0400

    [#12] try to move spark init to separate file

--- data_exploration/class_distribution.py
+++ data_exploration/class_distribution.py
@@ -1,22 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pyspark
-from pyspark.sql import SparkSession
-
-AITA_DB_NAME = 'aitaDB'
-AITA_POSTS_COLLECTION = 'extractedinfo'
-MONGODB_URI = 'mongodb://127.0.0.1/{}.{}'.format(AITA_DB_NAME, AITA_POSTS_COLLECTION)
-
-def init_spark():
-    return SparkSession \
-        .builder \
-        .appName('AITA Class Distribution') \
-        .master('local') \
-        .config('spark.driver.host','127.0.0.1') \
-        .config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.11:2.4.1') \
-        .config('spark.mongodb.input.uri', MONGODB_URI) \
-        .config('spark.mongodb.output.uri', MONGODB_URI) \
-        .getOrCreate()
+from spark.init_spark import init_spark
 
 def class_distribution():
     spark = init_spark()

796aec7956fbbebff216832c9d093e9f997dedca
Justin Cotarla <justin-cotarla@users.noreply.github.com>
2020-04-06 12:59:35 -0400

    Add feature scaling (#41)

--- ml/pipeline.py
+++ ml/pipeline.py
@@ -1,5 +1,6 @@
 import inspect
 
+from pyspark.ml.feature import MinMaxScaler
 
 class AITAPipeline:
     def __init__(self, spark, fix_imbalance=True):
@@ -44,6 +45,18 @@ class AITAPipeline:
         return self
 
     
+    def scale_data(self, data):
+        """Scales feature data by remapping the range to [0, 1]
+
+        Returns:
+            DataFrame -- Dataframe representation of an article
+        """
+        scaler = MinMaxScaler(inputCol="features", outputCol="scaledFeatures")
+        scaler_model = scaler.fit(data)
+        scaled_data = scaler_model.transform(data)
+
+        return scaled_data.drop("features").withColumnRenamed("scaledFeatures", "features")
+
     def run(self):
         """Runs the pipeline
         
@@ -53,11 +66,13 @@ class AITAPipeline:
         
         if not hasattr(self, '_dataset') or not hasattr(self, '_transformer') or not hasattr(self, '_classifier'):
             raise AttributeError('Please specify a dataset, a data transformer and a classifier')
-
+        
         self._transformer = self._transformer(self._spark, self._dataset)
         data = self._transformer.transform()
         
-        self._classifier = self._classifier(data)
+        scaled_data = self.scale_data(data)
+
+        self._classifier = self._classifier(scaled_data)
         self._classifier.train()
 
         return self._classifier.evaluate()
